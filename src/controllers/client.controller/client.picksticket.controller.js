481639cc13d466c1b2b142d1fe8bd2976c9cd6b3
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-17 01:11:40 +0700

    delete filter on server, it will write on font-end

--- src/controllers/client.controller/client.picksticket.controller.js
+++ src/controllers/client.controller/client.picksticket.controller.js
@@ -9,13 +9,13 @@ import Chuyen from '../../models/chuyenxe.model.js';
  * Phương thức get toàn bộ danh sách chuyến xe có thể đăng ký từ thời điểm truy cập
  * Phương thức xử lý bất đồng bộ async/await
  * @param {Object} req- Đối tượng request
- * @param {Object} res - Đối tượng Response 
+ * @param {Object} res - Đối tượng Response
  */
 export async function _getAllAvaiableChuyen(req, res) { // lấy danh sách chuyến xe còn chỗ để bắn ra cho người đăng ký
     try {
         const list = await Chuyen.find({}).populate({
             path: 'trips',
-            match: { timeStart: { $gte: new Date() } }
+            match: { timeStart: { $gte: Date.now() } }
         });
         return res.status(HTTPStatus.OK).json({ err: false, result: list });
     } catch (err) {
@@ -23,14 +23,5 @@ export async function _getAllAvaiableChuyen(req, res) { // lấy danh sách chuy
     }
 
 }
-export async function _filterAvaiableChuyen(req, res) {
-    try {
-        const filter= {
-            theoNgay: Date.parse(req.body.theoNgay) || '',
-            theoLotrinh:
-        }
-    } catch (err) {
-        return res.status(HTTPStatus.BAD_REQUEST).json({ err: true, message: 'Xuat hien loi tu thao tac cua ban' });
-    }
+// Lọc và tìm chuyến dựa vào reactjs xử lý
 
-}
