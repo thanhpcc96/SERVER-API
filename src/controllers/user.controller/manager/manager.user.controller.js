000cacdf70253373a845f2f99bd3418f34aea1fe
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-19 15:08:35 +0700

    Thêm Chức năng quản lý nhân viên

---
+++ src/controllers/user.controller/manager/manager.user.controller.js
@@ -0,0 +1,25 @@
+import HTTPStatus from 'http-status'
+import User from '../../../models/user.model';
+/**
+ * Hàm thực hiện load danh sách user
+ * Chỉ quản lý nhân sự và Người có quyên cao hơn được phép 
+ * Thực hiện quyền này
+ * @param {Object} req 
+ * @param {Object} res 
+ */
+export async function _getAllUser(req, res) {
+    try {
+        const user = req.user;
+        if (user.role !== 1 || user.role !== 2) {
+            return res.status(HTTPStatus.FORBIDDEN).json({ err: true, message: "Ban khong co quyen lam thuc hien chuc nang nay" });
+        }
+        const listUser = await User.find({ role: { $gt: 2 } });
+        if (!listUser) {
+            return res.status(HTTPStatus.NOT_FOUND).json({ err: true, message: "Xuat hien loi khi thuc hien yeu cau" });
+        }
+        return res.status(HTTPStatus.OK).json({ err: false, result: listUser });
+    } catch (err) {
+        return res.status(HTTPStatus.BAD_REQUEST).json({ err: true, message: "Loi tu yeu cau cua ban" });
+    }
+
+}
