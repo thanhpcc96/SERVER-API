403aa814c05b0347b8aed405cc830cde15273257
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-20 00:20:01 +0700

    Ok merge phat

--- src/controllers/user.controller/manager/manager.user.controller.js
+++ src/controllers/user.controller/manager/manager.user.controller.js
@@ -22,14 +22,49 @@ export async function _getAllUser(req, res) {
         return res.status(HTTPStatus.BAD_REQUEST).json({ err: true, message: "Loi tu yeu cau cua ban" });
     }
 }
-
+/**
+ * Hàm thực hiện xóa nhân viên
+ * Chỉ cho phép xóa từng người
+ * Chỉ có quản lý nhân sự và admin được xóa
+ * @param {Object} req 
+ * @param {Object} res 
+ */
 export async function _deleteUser(req, res) {
     try {
         const user = req.user;
         if (user.role !== 1 || user.role !== 2) {
             return res.status(HTTPStatus.FORBIDDEN).json({ err: true, message: " Ban khong co quyen thuc hien hanh dong nay" })
         }
+        return res.status(HTTPStatus.CONTINUE).json({ err: false, result: await User.findByIdAndRemove(req.body.userid) });
 
+    } catch (err) {
+        return res.status(HTTPStatus.BAD_REQUEST).json({ err: true, message: " Phat sinh loi tu hanh dong cua ban" });
+    }
+}
+
+export async function _postCreateUser(req, res){
+    try {
+        const user = req.user;
+        if (user.role !== 1 || user.role !== 2) {
+            return res.status(HTTPStatus.FORBIDDEN).json({ err: true, message: " Ban khong co quyen thuc hien hanh dong nay" })
+        }
+        const newUser={
+            email: req.body.email,
+            username: req.body.username,
+            password: '123456',
+            info: {
+                firstname: req.body.firstname,
+                lastname: req.body.lastname,
+                address: req.body.address,
+                passportNumber: req.body.passportNumber,
+                phoneNumber: req.body.phoneNumber,
+                photoProfile: {
+                    type: Array
+                },
+        
+            }
+
+        }
     } catch (err) {
         return res.status(HTTPStatus.BAD_REQUEST).json({ err: true, message: " Phat sinh loi tu hanh dong cua ban" });
     }
