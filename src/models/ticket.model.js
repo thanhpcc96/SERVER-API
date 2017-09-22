3e29f20045bf74813da63f8846451623567dfe62
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-23 00:22:32 +0700

    add medthod and middleware to  ticket model de xu ly logical tang thap nhat

--- src/models/ticket.model.js
+++ src/models/ticket.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from 'mongoose';
+import ChuyenxeModel from './chuyenxe.model';
 
 const TicketSchema = new Schema({
     codeTicket: {
@@ -7,7 +8,7 @@ const TicketSchema = new Schema({
     },
     price: {
         type: Number,
-        min:0
+        min: 0
     },
     dateOfStart: {
         type: Date
@@ -20,32 +21,51 @@ const TicketSchema = new Schema({
             type: String
         }
     },
-    inCoach:{
+    inChuyenXe: {
         type: Schema.Types.ObjectId,
-        ref: 'coachs'
+        ref: 'chuyenxe'
     },
     Customer: {
         type: Schema.Types.ObjectId,
         ref: 'clients'
     },
-    coupon:{
+    coupon: {
         type: Schema.Types.ObjectId,
-        ref:'coupons'
+        ref: 'coupons'
     },
-    isPayed:{
+    isAvaiable: { // Ve nay co bi huy khong? true hoac false // true la bi huy ve
+        type: Boolean,
+        default: false
+    },
+    isPayed: {
         type: Boolean,
         default: false
     }
 }, { timestamps: true });
 
 
-// TicketSchema.methods={
-//  /**
-//   * Update tien khi add conpoun
-//   */
-//   _UpdatePriceWithCoupon(){
-      
-//   }
-// }
 
-export default mongoose.model('tickets',TicketSchema);
+TicketSchema.pre('save', function (next) {
+    if (this.isModified('isAvaiable')) {
+        if (this.isAvaiable === true) {
+            this._removeTicketFromChuyenXe();
+        }
+    }
+    return next();
+});
+
+
+TicketSchema.methods = {
+    /**
+     *  Ham xu ly hanh dong khach hang huy chuyen xe
+     */
+    async _removeTicketFromChuyenXe() {
+        const chuyenxe = await ChuyenxeModel.findById(this.inChuyenXe);
+        if (!chuyenxe) return;
+        if (chuyenxe.ticketsInChuyen.indexOf(this._id)) {
+            chuyenxe.ticketsInChuyen.remove(this._id);
+        }
+    }
+}
+
+export default mongoose.model('tickets', TicketSchema);
