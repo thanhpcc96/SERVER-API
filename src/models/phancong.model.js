edebcd0518d90bbbadc236e090da6f0988631112
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-24 00:03:33 +0700

    fix xong logic error

--- src/models/phancong.model.js
+++ src/models/phancong.model.js
@@ -1,1 +1,24 @@
-import 
+import mongoose, { Schema } from "mongoose";
+
+const phancongSchema = new Schema({
+    ngayphancong: {
+        type: Date,
+        default: Date.now()
+    },
+    khunggio: {
+        type: String, // chỉ có MORNING và hoặc AFTERNOON
+        default: 'MORNING'
+    },
+    trips: {
+        type: Schema.Types.ObjectId,
+        ref: 'trips'
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    coach: {
+        type: Schema.Types.ObjectId,
+        ref: 'coachs'
+    }
+})
--- src/tasks/refesh.seat.day.js
+++ src/tasks/refesh.seat.day.js
@@ -7,6 +7,9 @@ import tripModel from '../models/trips.model';
 export default function refeshSeat(agenda){
     agenda.define("refeshseat",(job, done)=>{
         let time= Date.now();
+        time= time.setHours(6,0,0,0);
+        
+
         for(let i=0; i<6; i++){
             
         }
