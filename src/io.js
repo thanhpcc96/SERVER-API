f29843076ba180cbeb9fc01cce861c14d97e6b16
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-28 23:08:11 +0700

    Nên bắn dữ liệu ra redis không nhỉ?

--- src/io.js
+++ src/io.js
@@ -1,3 +1,18 @@
 import socketIO from 'socket.io';
+
 const io = socketIO();
+let countClient = 0;
+
+/**
+ *  Test streaming tren defaut path '/'
+ */
+io.on('connection', socket => {
+    countClient = countClient + 1;
+    socket.emit('countlient', countClient);
+});
+
+const clients = io.of('/client').on('connection', socket => {
+    cl
+})
+
 export default io;
---
+++ src/streaming/client.js
@@ -0,0 +1,7 @@
+import ChuyenxeModel from '../models/chuyenxe.model';
+
+export function _getListChuyenXe(){
+    return new Promise((resolve,reject)=>{
+        // nen ban data vao redis khong nhi?
+    })
+}
