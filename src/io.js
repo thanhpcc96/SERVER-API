507270645950b9fbb9d6358985ee97ca529dac19
Thanh Pham <thanhpcc1996@gmail.com>
2017-09-30 23:01:28 +0700

    clear fix websocket

--- src/io.js
+++ src/io.js
@@ -1,13 +1,14 @@
 /*eslint-disable */
 import socketIO from 'socket.io';
 import redis from 'redis';
-import adapter from 'socket.io-redis'
+import adapter from 'socket.io-redis';
+import chuyenxeModel from './models/chuyenxe.model';
 
 
 const io = socketIO();
 
 // Ep Socket.io chi su dung duy nhat websocket. Khong su dung ket noi lau hon.
-io.set('transports', ['websocket']);
+//io.set('transports', ['websocket']);
 
 const pubClient = redis.createClient();
 const subClient = redis.createClient({ return_buffers: true });
@@ -19,6 +20,41 @@ io.adapter(adapter({ pubClient, subClient }));
  * Có lên sử dụng session store không nhỉ?
  * Chắc không! haha đã pubsub rồi bắn store làm mẹ gì? are you ok?
  */
+
+
+/**
+ *  Them cac io event
+ */
+io.of('/client').on('connection', socket => {
+
+    const listchuyen = [];
+
+    // load chuyen xe co the dang ky duoc tu thoi gian hien tai
+    socket.on('loadchuyenxe', () => {
+        chuyenxeModel.find({})
+            .populate({
+                path: 'trips',
+                match: { timeStart: { $gte: Date.now() } }
+            }).
+            exec((err, result) => {
+                if (err) {
+                    socket.emit('updateListChuyenxe', {err: "Không load được list Chuyen xe"});
+                }
+                console.log('===============================');
+                console.log(result);
+                console.log('===============================');
+                result.forEach(item => {
+                    listchuyen.push(item);
+                });
+            });
+        socket.emit('updateListChuyenxe', listchuyen);
+        socket.broadcast.emit('updateListChuyenxe', listchuyen);
+    });
+
+    // pick chuyen
+
+    socket.on()
+})
 
 
 export default io;
