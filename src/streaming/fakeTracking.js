a0429513709909197278c52b78a280129deddedc
Thanh Pham <thanhpcc1996@gmail.com>
2017-10-02 10:45:51 +0700

    Fake tracking Location 70%

--- src/streaming/fakeTracking.js
+++ src/streaming/fakeTracking.js
@@ -3,10 +3,11 @@ import fs from 'fs';
 
 let Socket = null;
 
+const dirName = 'gpx-data/';
 let trackingState = {};
 
 /**
- * H
+ * Hàm emit trạng thái tracking location qua socket
  * @param {String} routeName 
  * @param {Array} routeNodes 
  * @param {Number} currentIndex 
@@ -30,3 +31,53 @@ const emitRoute = (routeName, routeNodes, currentIndex) => {
         emitRoute(routeName, routeNodes, ++currentIndex);
     }, delayTime);
 }
+/**
+ * Đọc tọa độ của file gpx và tracking lên map và emit về cho client
+ * @param {socket} io - Đối tượng socket
+ */
+/* eslint-disable no-console */
+const run = io => {
+    Socket = io;
+    fs.readdir(dirName, (err, filenames) => {
+        if (err) {
+            console.log('===============================');
+            console.log(err);
+            console.log('===============================');
+            return;
+        }
+        filenames.forEach(filename => {
+            // Kiểm tra định dạng file, chỉ chấp nhận *.gpx( đinh dạng file trich xuất tọa độ của google map)
+            const fileExtension = filename.split('.').pop();
+            if (fileExtension !== 'gpx') return;
+            fs.readFile(dirName + filename, 'utf-8', (e, content) => {
+                if (e) {
+                    console.log('===============================');
+                    console.log(e);
+                    console.log('===============================');
+                    return;
+                }
+                // đẩy nội dung của file gpx vào trackingState
+                trackingState = Object.assign(trackingState, {
+                    [filename]: { lat: 0, lng: 0 }
+                });
+                // Parse XML với cheerio
+                const $ = cheerio.load(content, {
+                    normalizeWhitespace: true,
+                    xmlMode: true
+                });
+
+                // chuyển đổi xmlNode object sang object trong js {lat: ..., lng: ...} 
+                const routeNodes = $('wpt').map((i, node) => ({
+                    lat: Number($(node).attr('lat')),
+                    lng: Number($(node).attr('lon'))
+                })).get();
+
+                // emit route to client
+                if (routeNodes.length > 0) {
+                    emitRoute(filename, routeNodes, 0)
+                }
+            });
+        });
+    });
+}
+export default run;
