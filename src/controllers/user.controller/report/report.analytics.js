19fd191f2eee5963a74c50d216dead19310bd889
Thanh Pham <thanhpcc1996@gmail.com>
2017-10-07 22:34:42 +0700

    add chức năng xem báo cáo doanh thu

--- src/controllers/user.controller/report/report.analytics.js
+++ src/controllers/user.controller/report/report.analytics.js
@@ -1,14 +1,35 @@
 import HTTPStatus from 'http-status';
+import moment from 'moment';
+
+import TicketModel from '../../../models/ticket.model';
+import ChuyenxeModel from '../../../models/chuyenxe.model';
 
 /**
  *  Hàm get doanh thu tạm thời theo ngày
+ *  thống kê số lượng vé giữ chỗ, số lượng vé đặt và thanh toán
+ *  Số lượng khách tham gia chuyến, chưa tham gia,..
  * @param {Object} req 
  * @param {Object} res 
  */
-export async function _getReportRevenueDay(req, res){
- try {
-     // to do something
- } catch (err) {
-     return res.status(HTTPStatus.BAD_REQUEST).json({err: true, message: 'Loi phat sinh tu hanh dong cua ban'});
- }
+export async function _getReportRevenueDay(req, res) {
+    try {
+        const now = moment();
+        /* Load ra promise voi async va await */
+        const listPromise = await Promise.all([
+            /* Load những vé đăng ký trong ngày */
+            TicketModel.find({}).populate({
+                path: 'inChuyenXe',
+                match: { timeStart: { $gt: now.set({ hour: 5, minute: 0 }) } }
+            }),
+            /* Load những chuyến xe và thông tin từng chuyến xe trong trong ngày */
+            ChuyenxeModel.find({
+                timeStart: { $gt: now.set({ hour: 5, minute: 0 }) }
+            }).populate('routeOfTrip')
+                .populate('ticketsInChuyen'),
+
+        ]);
+        
+    } catch (err) {
+        return res.status(HTTPStatus.BAD_REQUEST).json({ err: true, message: 'Loi phat sinh tu hanh dong cua ban' });
+    }
 }
