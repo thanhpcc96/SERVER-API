77f5de6d4aae0e69c95ebe938c4637fa28685bdc
Thanh Pham <thanhpcc1996@gmail.com>
2017-10-07 12:24:56 +0700

    test thuat toan moi job queue

--- src/tasks/copy.gpx.file.js
+++ src/tasks/copy.gpx.file.js
@@ -1,9 +1,20 @@
 import fs from 'fs';
+import moment from 'moment';
 
 import chuyenxeModel from '../models/chuyenxe.model';
+import lotrinhModel from '../models/lotrinh.model';
 
-export default function copyGPXfile(agenda){
-    agenda.define('copyfile',(job, done)=>{
-        
+export default function copyGPXfile(agenda) {
+    agenda.define('copyfile', (job, done) => {
+        const now = moment();
+        chuyenxeModel.find({
+            timeStart: { $gt: now.set({ hour: 0, minute: 5 }), $lt: now.set({ hour: 20, minute: 30 }) }
+        })
+        .populate('routeOfTrip')
+        .exec((err,result)=>{
+            if(err){ return }
+            if(re)
+        })
+
     });
 }
